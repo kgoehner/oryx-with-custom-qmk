c913f558b4056e5d93daa4a6e4d2db7f8c6a5fdd
Kyle Goehner <kgoehner@amazon.com>
2025-02-20 23:24:45 -0500

    Add repeat key

--- mQ5mx/keymap.c
+++ mQ5mx/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_BSPC,        LT(1,KC_A),     MT(MOD_LALT, KC_S),MT(MOD_LSFT, KC_D),LT(2,KC_F),     KC_G,                                           KC_H,           LT(3,KC_J),     MT(MOD_RSFT, KC_K),MT(MOD_RALT, KC_L),LT(1,KC_SCLN),  KC_QUOTE,       
     CW_TOGG,        MT(MOD_LGUI, KC_Z),KC_X,           KC_C,           MT(MOD_LCTL, KC_V),KC_B,                                           KC_N,           MT(MOD_RCTL, KC_M),KC_COMMA,       KC_DOT,         MT(MOD_RGUI, KC_SLASH),KC_ENTER,       
-                                                    KC_SPACE,       LT(4,KC_ESCAPE),                                KC_ESCAPE,      KC_TRANSPARENT
+                                                    KC_SPACE,       LT(4,KC_ESCAPE),                                KC_ESCAPE,      QK_REP
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 LCTL(KC_PGDN),  KC_PGDN,        KC_PAGE_UP,     LCTL(KC_PAGE_UP),CW_TOGG,        KC_AUDIO_MUTE,  
