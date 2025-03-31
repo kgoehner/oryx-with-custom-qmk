8af0e88319ca106961dfe7c2b1a12eec1dee606b
Kyle Goehner <KyleA.Goehner@gmail.com>
2025-03-30 20:01:34 -0400

    Add new layer 4 key

--- mQ5mx/keymap.c
+++ mQ5mx/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,       ST_MACRO_0,     KC_MAC_PASTE,   LCTL(KC_A),     KC_MAC_COPY,    KC_MAC_CUT,                                     KC_HOME,        KC_LEFT,        KC_RIGHT,       KC_END,         LGUI(KC_TAB),   KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_BSPC,        LT(1,KC_A),     MT(MOD_LALT, KC_S),MT(MOD_LSFT, KC_D),LT(2,KC_F),     KC_G,                                           KC_H,           LT(3,KC_J),     MT(MOD_RSFT, KC_K),MT(MOD_RALT, KC_L),LT(1,KC_SCLN),  KC_QUOTE,       
-    CW_TOGG,        MT(MOD_LGUI, KC_Z),KC_X,           KC_C,           MT(MOD_LCTL, KC_V),KC_B,                                           KC_N,           MT(MOD_RCTL, KC_M),KC_COMMA,       KC_DOT,         MT(MOD_RGUI, KC_SLASH),KC_ENTER,       
+    CW_TOGG,        MT(MOD_LGUI, KC_Z),KC_X,           KC_C,           MT(MOD_LCTL, KC_V),KC_B,                                           KC_N,           MT(MOD_RCTL, KC_M),KC_COMMA,       LT(4, KC_DOT),         MT(MOD_RGUI, KC_SLASH),KC_ENTER,
                                                     TD(DANCE_0),    KC_SPACE,                                       QK_REP, KC_ESCAPE
   ),
   [1] = LAYOUT_voyager(
